ca2d92af6512c156811975dc3164fe9a68056695
DawnT0wn <756231955@qq.com>
2023-06-04 11:57:03 +0800

    patient页面渲染

--- his_vuecli/src/views/Home.vue
+++ his_vuecli/src/views/Home.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="home">
+      <div class="tile_list">
+          <el-tabs v-model="activeName" @tab-click="handleClick">
+              <el-tab-pane label="医院简介" name="first">
+                  <div class="outline-image">
+                      <el-image src="" fit="contain"></el-image>
+                      <el-image src="" fit="contain"></el-image>
+                  </div>
+              </el-tab-pane>
+              <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+              <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+              <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+          </el-tabs>
+      </div>
+
         <div class="home_title">
             <div class="home_img">
                 <img src="../assets/Choose/HOME1.png" alt="" width="300px">
@@ -22,10 +36,16 @@
 
 export default {
   name: 'Home',
-  components: {
-
-  },
+  components: {},
+    data() {
+        return {
+            activeName: 'second'
+        };
+    },
     methods:{
+        handleClick(tab, event) {
+            console.log(tab, event);
+        },
         godoctorlogin(){
             this.$router.push('/DoctorLogin')
         },
@@ -59,6 +79,15 @@ export default {
         left: 40px;
         top: 400px;
     }
-
-
+    .tile_list{
+        position: absolute;;
+        left: 50px;
+        top: 20px;
+    }
+    .outline-image {
+        width: 300px; /* 设置轮廓图容器的宽度 */
+        height: 300px; /* 设置轮廓图容器的高度 */
+        display: flex; /* 设置轮廓图容器为弹性盒子 */
+        justify-content: space-between; /* 图片之间的间距 */
+    }
 </style>
