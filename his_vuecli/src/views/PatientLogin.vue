bb97a055fa0ca57ec04c29c1d931b475a8fac0ad
CaSO44444 <752156917@qq.com>
2023-06-06 16:36:34 +0800

    增加News页面，Doctors页面，去除部分无用代码，完善patient功能，修复bug

--- his_vuecli/src/views/PatientLogin.vue
+++ his_vuecli/src/views/PatientLogin.vue
@@ -86,51 +86,6 @@ export default {
           });
             this.flushcode();
         }
-        // if(this.inf === "管理员"){
-        //     this.$router.push({
-        //         path:'/AdminHome' ,               	//目标URL，为注册的路由
-        //
-        //         query:{
-        //             username:this.form.username      	//传入参数，参数会写入URL，params用法同query，只不过参数不会写进URL
-        //         }
-        //     })
-        // }
-        // if(this.inf === "收费员"){
-        //     this.$router.push({
-        //         path:'/CashierHome' ,               	//目标URL，为注册的路由
-        //
-        //         query:{
-        //             username:this.form.username      	//传入参数，参数会写入URL，params用法同query，只不过参数不会写进URL
-        //         }
-        //     })
-        // }
-        // if(this.inf === "检验医生"){
-        //     this.$router.push({
-        //         path:'/DoctorTestHome' ,               	//目标URL，为注册的路由
-        //
-        //         query:{
-        //             username:this.form.username      	//传入参数，参数会写入URL，params用法同query，只不过参数不会写进URL
-        //         }
-        //     })
-        // }
-        // if(this.inf === "检查医生"){
-        //     this.$router.push({
-        //         path:'/DoctorInspectionHome' ,               	//目标URL，为注册的路由
-        //
-        //         query:{
-        //             username:this.form.username      	//传入参数，参数会写入URL，params用法同query，只不过参数不会写进URL
-        //         }
-        //     })
-        // }
-        // if(this.inf === "药房医生"){
-        //     this.$router.push({
-        //         path:'/DoctorDrugHome' ,               	//目标URL，为注册的路由
-        //
-        //         query:{
-        //             username:this.form.username      	//传入参数，参数会写入URL，params用法同query，只不过参数不会写进URL
-        //         }
-        //     })
-        // }
         if(this.inf.code === 500){
           this.$alert('', '登陆失败 账号密码错误', {
             confirmButtonText: '确定',
